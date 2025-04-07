add137b3343db0b4632e67ee3136a90e4a1ffe00
Helena Skagerlid <helenaskagerlid@gmail.com>
2025-04-07 09:47:18 +0200

    add content and collection for pages

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,20 +1,25 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
-import { glob } from 'astro/loaders';
-
+import { glob } from "astro/loaders";
 
 const indices = defineCollection({
-  loader: glob({ pattern: ['*.{md,mdx}'], base: 'src/content/indices' }),
+  loader: glob({ pattern: ["*.{md,mdx}"], base: "src/content/indices" }),
   schema: () =>
     z.object({
       name: z.string(),
       indexName: z.string(),
-      indexAcronym: z.string()
+      indexAcronym: z.string(),
     }),
 });
 
-
+const pages = defineCollection({
+  loader: glob({ pattern: ["*.{md,mdx}"], base: "src/content/pages" }),
+  schema: () =>
+    z.object({
+      title: z.string(),
+    }),
+});
 
 export const collections = {
-  indices
+  indices,
 };
