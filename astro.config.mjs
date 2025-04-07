08cb3b70e67e259a4cfafc0118f3908f0bdbb65f
Helena Skagerlid <helenaskagerlid@gmail.com>
2025-04-07 22:48:59 +0200

    install katex and wip apply katex in indices

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    syntaxHighlight: "prism",
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
   experimental: {
-      svg: true,
-    },
+    svg: true,
+  },
 
   integrations: [mdx()],
 });
