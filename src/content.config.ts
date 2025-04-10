3216c4a187074f25118b6875e876665089d181c6
Helena Skagerlid <helenaskagerlid@gmail.com>
2025-04-10 19:05:45 +0200

    wip endpoint for breakpoints

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,7 @@
 import { defineCollection, z } from "astro:content";
 import { file, glob } from "astro/loaders";
 import { parse } from "csv-parse/sync";
-import fs from "node:fs";
+import { globSync } from "tinyglobby";
 
 const indices = defineCollection({
   loader: glob({ pattern: ["*.{md,mdx}"], base: "src/content/indices" }),
@@ -22,8 +22,7 @@ const pages = defineCollection({
 });
 
 const filePaths = "src/data/breakpoints";
-
-const allFiles = fs.readdirSync(filePaths);
+const allFiles = globSync(`${filePaths}/*.csv`);
 
 const breakpoints = allFiles.map((o: string) => {
   return defineCollection({
--- src/pages/api/data/[indice].json.ts
+++ src/pages/api/data/[indice].json.ts
@@ -20,3 +20,26 @@ export const GET: APIRoute = async ({ params }) => {
 export function getStaticPaths() {
   return [{ params: { indice: "eu" } }];
 }
+
+// import type { APIRoute } from "astro";
+// import { parse } from "csv-parse/sync";
+// import path from "node:path";
+
+// export const GET: APIRoute = async ({ params }) => {
+//   const indice = params.indice;
+
+//   const filePath = path.join("src/data/breakpoints", `${indice}.csv`);
+
+//   const parsedContent = parse(filePath, {
+//     columns: true,
+//     skip_empty_lines: true,
+//   });
+
+//   return new Response(JSON.stringify(parsedContent), {
+//     headers: { "Content-Type": "application/json" },
+//   });
+// };
+
+// export function getStaticPaths() {
+//   return [{ params: { indice: "eu" } }];
+// }
