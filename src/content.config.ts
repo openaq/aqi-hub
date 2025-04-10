907c8a5a04bf6fe889d7c697f3a5ea0c4217aaa4
Helena Skagerlid <helenaskagerlid@gmail.com>
2025-04-10 18:20:31 +0200

    wip endpoint to fetch and parse csv

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,7 @@
 import { defineCollection, z } from "astro:content";
-import { glob } from "astro/loaders";
+import { file, glob } from "astro/loaders";
+import { parse } from "csv-parse/sync";
+import fs from "node:fs";
 
 const indices = defineCollection({
   loader: glob({ pattern: ["*.{md,mdx}"], base: "src/content/indices" }),
@@ -19,19 +21,20 @@ const pages = defineCollection({
     }),
 });
 
-const breakpoints = defineCollection({
-  loader: glob({
-    pattern: "*.csv",
-    base: "src/data/breakpoints",
-  }),
-  schema: () =>
-    z.object({
-      ISO: z.string(),
+const filePaths = "src/data/breakpoints";
+
+const allFiles = fs.readdirSync(filePaths);
+
+const breakpoints = allFiles.map((o: string) => {
+  return defineCollection({
+    loader: file(o, {
+      parser: (text) => parse(text, { columns: true, skipEmptyLines: true }),
     }),
+  });
 });
 
 export const collections = {
-  breakpoints,
   indices,
   pages,
+  ...breakpoints,
 };
