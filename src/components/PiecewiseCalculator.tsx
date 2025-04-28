8ddfaa000dd55029d636f60a858c1219e55a7fb1
Helena Skagerlid <helenaskagerlid@gmail.com>
2025-04-28 21:49:22 +0200

    refactor PiecewiseCalculator + styling

--- src/components/PiecewiseCalculator.tsx
+++ src/components/PiecewiseCalculator.tsx
@@ -11,14 +11,13 @@ interface PiecewiseCalculatorDefinition {
 const latexForumula = piecewiseFunction("AQI");
 
 const PiecewiseCalculator = (props: PiecewiseCalculatorDefinition) => {
-  const [valuePm25, setValuePm25] = createSignal(0);
-  const [valuePm10, setValuePm10] = createSignal(0);
-  const [pm25Result, setPm25Result] = createSignal(0);
-  const [pm10Result, setPm10Result] = createSignal(0);
+  const [value, setValue] = createSignal(0);
+  const [result, setResult] = createSignal(0);
+
   const [hexCode, setHexCode] = createSignal("");
   const [numberCalculation, setNumberCalculation] = createSignal("");
 
-  const calculate = (pollutant: "PM2.5" | "PM10", value: number) => {
+  const calculate = (value: number) => {
     const indexValue = props.data.find((o: IndexDefinition) => {
       const concentrationUpper = o.concentrationUpper ?? 500;
       return value >= o.concentrationLower && value <= concentrationUpper;
@@ -50,62 +49,34 @@ const PiecewiseCalculator = (props: PiecewiseCalculatorDefinition) => {
     setNumberCalculation(piecewiseFunctionWithNumbers("AQI", parameters));
 
     setHexCode(indexValue.hex);
-
-    if (pollutant === "PM2.5") {
-      setPm25Result(Math.round(result));
-    } else if (pollutant === "PM10") {
-      setPm10Result(Math.round(result));
-    }
+    setResult(Math.round(result));
   };
 
-  const handlePm25Input = (e: Event) => {
+  const handleInput = (e: Event) => {
     const newValue = Number((e.currentTarget as HTMLInputElement).value);
-    setValuePm25(newValue);
-    calculate("PM2.5", newValue);
-  };
-
-  const handlePm10Input = (e: Event) => {
-    const newValue = Number((e.currentTarget as HTMLInputElement).value);
-    setValuePm10(newValue);
-    calculate("PM10", newValue);
+    setValue(newValue);
+    calculate(newValue);
   };
 
   return (
     <>
-      <Show when={props.pollutant === "PM2.5"}>
+      <div class="calculation-wrapper">
         <label>{props.pollutant}</label>
-        <input type="number" value={valuePm25()} onInput={handlePm25Input} />
-        <div class="calculation-wrapper">
-          <div innerHTML={numberCalculation()}></div>
-          <Show when={!numberCalculation()}>
-            <div innerHTML={latexForumula}></div>
-          </Show>
-          <p>= {pm25Result()}</p>
-          <div class="result-wrapper">
-            <div
-              class="color-box"
-              style={{ "background-color": hexCode() }}
-            ></div>
-          </div>
+        <input type="number" value={value()} onInput={handleInput} />
+        <Show
+          when={!numberCalculation() || value() <= 0}
+          fallback={<div innerHTML={numberCalculation()}></div>}
+        >
+          <div innerHTML={latexForumula}></div>
+        </Show>
+        <p>= {result()}</p>
+        <div class="result-wrapper">
+          <div
+            class="color-box"
+            style={{ "background-color": hexCode() }}
+          ></div>
         </div>
-      </Show>
-      <Show when={props.pollutant === "PM10"}>
-        <label>{props.pollutant}</label>
-        <input type="number" value={valuePm10()} onInput={handlePm10Input} />
-        <div class="calculation-wrapper">
-          <div innerHTML={numberCalculation()}></div>
-          <Show when={!numberCalculation()}>
-            <div innerHTML={latexForumula}></div>
-          </Show>
-          <p>= {pm10Result()}</p>
-          <div class="result-wrapper">
-            <div
-              class="color-box"
-              style={{ "background-color": hexCode() }}
-            ></div>
-          </div>
-        </div>
-      </Show>
+      </div>
     </>
   );
 };
