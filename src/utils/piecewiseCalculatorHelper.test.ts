3aaf761d67efdc4757fe267c57e50d659f1d97a4
Helena Skagerlid <helenaskagerlid@gmail.com>
2025-05-16 13:39:49 +0200

    wip testing PiecewiseCalculator functions
    Co-authored-by: Rebecka Göthlin <RebeckaGothlin@users.noreply.github.com>

--- src/utils/piecewiseCalculatorHelper.test.ts
+++ src/utils/piecewiseCalculatorHelper.test.ts
@@ -1,90 +1,94 @@
 import type { IndexDefinition } from "src/types/types";
 import { describe, expect, test, vi } from "vitest";
-import { generateLatexFunction, getResult } from "./piecewiseCalculatorHelpers";
+import {
+  generateLatexFunction,
+  getResult,
+  indexValue,
+} from "./piecewiseCalculatorHelpers";
 import * as piecewise from "./piecewiseFunction";
 
-describe("getResult", () => {
-  const data: IndexDefinition[] = [
-    {
-      iso: "US",
-      variant: "",
-      category: "Good",
-      hex: "#00e400",
-      categoryLower: 0,
-      categoryUpper: 50,
-      pollutant: "PM2.5",
-      units: "ug/m3",
-      averagingPeriod: 24,
-      concentrationLower: 0,
-      concentrationUpper: 9,
-    },
-    {
-      iso: "US",
-      variant: "",
-      category: "Moderate",
-      hex: "#fffe00",
-      categoryLower: 51,
-      categoryUpper: 100,
-      pollutant: "PM2.5",
-      units: "ug/m3",
-      averagingPeriod: 24,
-      concentrationLower: 9.1,
-      concentrationUpper: 35.4,
-    },
-    {
-      iso: "US",
-      variant: "",
-      category: "Unhealthy for Sensitive Groups",
-      hex: "#ff7e00",
-      categoryLower: 101,
-      categoryUpper: 150,
-      pollutant: "PM2.5",
-      units: "ug/m3",
-      averagingPeriod: 24,
-      concentrationLower: 35.5,
-      concentrationUpper: 55.4,
-    },
-    {
-      iso: "US",
-      variant: "",
-      category: "Unhealthy",
-      hex: "#ff0000",
-      categoryLower: 151,
-      categoryUpper: 200,
-      pollutant: "PM2.5",
-      units: "ug/m3",
-      averagingPeriod: 24,
-      concentrationLower: 55.5,
-      concentrationUpper: 125.4,
-    },
-    {
-      iso: "US",
-      variant: "",
-      category: "Very Unhealthy",
-      hex: "#8f3f97",
-      categoryLower: 201,
-      categoryUpper: 300,
-      pollutant: "PM2.5",
-      units: "ug/m3",
-      averagingPeriod: 24,
-      concentrationLower: 125.5,
-      concentrationUpper: 225.4,
-    },
-    {
-      iso: "US",
-      variant: "",
-      category: "Hazardous",
-      hex: "#7e0023",
-      categoryLower: 301,
-      categoryUpper: 0,
-      pollutant: "PM2.5",
-      units: "ug/m3",
-      averagingPeriod: 24,
-      concentrationLower: 225.5,
-      concentrationUpper: 0,
-    },
-  ];
+const data: IndexDefinition[] = [
+  {
+    iso: "US",
+    variant: "",
+    category: "Good",
+    hex: "#00e400",
+    categoryLower: 0,
+    categoryUpper: 50,
+    pollutant: "PM2.5",
+    units: "ug/m3",
+    averagingPeriod: 24,
+    concentrationLower: 0,
+    concentrationUpper: 9,
+  },
+  {
+    iso: "US",
+    variant: "",
+    category: "Moderate",
+    hex: "#fffe00",
+    categoryLower: 51,
+    categoryUpper: 100,
+    pollutant: "PM2.5",
+    units: "ug/m3",
+    averagingPeriod: 24,
+    concentrationLower: 9.1,
+    concentrationUpper: 35.4,
+  },
+  {
+    iso: "US",
+    variant: "",
+    category: "Unhealthy for Sensitive Groups",
+    hex: "#ff7e00",
+    categoryLower: 101,
+    categoryUpper: 150,
+    pollutant: "PM2.5",
+    units: "ug/m3",
+    averagingPeriod: 24,
+    concentrationLower: 35.5,
+    concentrationUpper: 55.4,
+  },
+  {
+    iso: "US",
+    variant: "",
+    category: "Unhealthy",
+    hex: "#ff0000",
+    categoryLower: 151,
+    categoryUpper: 200,
+    pollutant: "PM2.5",
+    units: "ug/m3",
+    averagingPeriod: 24,
+    concentrationLower: 55.5,
+    concentrationUpper: 125.4,
+  },
+  {
+    iso: "US",
+    variant: "",
+    category: "Very Unhealthy",
+    hex: "#8f3f97",
+    categoryLower: 201,
+    categoryUpper: 300,
+    pollutant: "PM2.5",
+    units: "ug/m3",
+    averagingPeriod: 24,
+    concentrationLower: 125.5,
+    concentrationUpper: 225.4,
+  },
+  {
+    iso: "US",
+    variant: "",
+    category: "Hazardous",
+    hex: "#7e0023",
+    categoryLower: 301,
+    categoryUpper: 0,
+    pollutant: "PM2.5",
+    units: "ug/m3",
+    averagingPeriod: 24,
+    concentrationLower: 225.5,
+    concentrationUpper: 0,
+  },
+];
 
+describe("getResult", () => {
   test("beräkning right index inom fööörst span", () => {
     const result = getResult(data, 5);
     expect(Math.round(result)).toBeCloseTo(28);
@@ -121,6 +125,16 @@ describe("getResult", () => {
 
     expect(result).toBeCloseTo(expected);
   });
+
+  test("returnerar categoryLower om concentration is exakt concentrationLower", () => {
+    const result = getResult(data, 35.5);
+    expect(Math.round(result)).toBe(101);
+  });
+
+  test("returnerar categoryUpper om contration is exakt concentrationUpper", () => {
+    const result = getResult(data, 35.4);
+    expect(Math.round(result)).toBe(100);
+  });
 });
 
 describe("generateLatexFunction", () => {
@@ -128,22 +142,6 @@ describe("generateLatexFunction", () => {
     vi.spyOn(piecewise, "piecewiseFunctionWithNumbers").mockReturnValue(
       "mockLatex"
     );
-
-    const data = [
-      {
-        iso: "US",
-        variant: "",
-        category: "Hazardous",
-        hex: "#7e0023",
-        categoryLower: 301,
-        categoryUpper: 0,
-        pollutant: "PM2.5",
-        units: "ug/m3",
-        averagingPeriod: 24,
-        concentrationLower: 225.5,
-        concentrationUpper: 0,
-      },
-    ];
 
     const result = generateLatexFunction(data, 400);
 
@@ -156,5 +154,17 @@ describe("generateLatexFunction", () => {
     );
 
     expect(result).toBe("mockLatex");
+  });
+});
+
+describe("indexValue", () => {
+  test("indexValue finds rätt interval baserat on concentration", () => {
+    const index = indexValue(data, 35);
+    expect(index?.category).toBe("Moderate");
+  });
+
+  test("indexValue returnerar undefined if no match hittas", () => {
+    const index = indexValue(data, -10);
+    expect(index).toBeUndefined();
   });
 });
