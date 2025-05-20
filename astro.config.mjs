dbc147a7e5fae778f66d05f21ce7c250cf736e4c
Russ Biggs <russbiggs@gmail.com>
2025-05-20 13:22:40 -0600

    site parameter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,37 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import remarkMath from "remark-math";
-import rehypeKatex from "rehype-katex";
-import rehypeSlug from "rehype-slug";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import mdx from "@astrojs/mdx";
-import rehypeExternalLinks from "rehype-external-links";
-import { remarkModifiedTime } from "./remark-modified-time.mjs";
+import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import mdx from '@astrojs/mdx';
+import rehypeExternalLinks from 'rehype-external-links';
+import { remarkModifiedTime } from './remark-modified-time.mjs';
 
-import solidJs from "@astrojs/solid-js";
+import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://aqihub.info',
   markdown: {
-    syntaxHighlight: "prism",
+    syntaxHighlight: 'prism',
     remarkPlugins: [remarkMath, remarkModifiedTime],
     rehypePlugins: [
       rehypeKatex,
       [
         rehypeExternalLinks,
-        { target: "_blank", rel: ["noopener", "noreferrer"] },
+        { target: '_blank', rel: ['noopener', 'noreferrer'] },
       ],
       rehypeSlug,
       [
         rehypeAutolinkHeadings,
         {
-          behavior: "wrap",
+          behavior: 'wrap',
           headingProperties: {
-            className: ["anchor"],
+            className: ['anchor'],
           },
           properties: {
-            className: ["anchor-link"],
+            className: ['anchor-link'],
           },
         },
       ],
