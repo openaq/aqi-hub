118f69e57fa31461965abe86799a29e6bcd8831e
Russ Biggs <russbiggs@gmail.com>
2025-05-20 19:00:14 -0600

    refactor data endpoints and create static data loaders

---
+++ src/data/loaders.ts
@@ -0,0 +1,51 @@
+import { readFileSync } from 'node:fs';
+import { parse } from 'csv-parse/sync';
+import { globSync } from 'tinyglobby';
+import type { IndexDefinition } from 'src/types/types';
+
+const snakeToCamel = (str: string) =>
+  str
+    .toLowerCase()
+    .replace(/([-_][a-z])/g, (group: string) =>
+      group.toUpperCase().replace('-', '').replace('_', '')
+    );
+
+
+  const stringToNumber =  (data) : IndexDefinition[] => data.map(
+    (o: IndexDefinition) => ({
+      ...o,
+      categoryLower: Number(o.categoryLower),
+      categoryUpper: Number(o.categoryUpper),
+      averagingPeriod: Number(o.averagingPeriod),
+      concentrationLower: Number(o.concentrationLower),
+      concentrationUpper: Number(o.concentrationUpper),
+    })
+  );
+
+export function loadIndex(index: string) {
+  const data = readFileSync(`src/data/breakpoints/${index}.csv`);
+
+  const parsedContent = parse(data, {
+    skip_empty_lines: true,
+    columns: (header) => header.map((column: string) => snakeToCamel(column)),
+  });
+  return stringToNumber(parsedContent)
+}
+export function loadIndices() {
+  const data = globSync(['src/data/breakpoints/*.csv']);
+
+  const parsedContent = data.map((o) => {
+    const csvContent = readFileSync(o);
+
+    const parsed = parse(csvContent, {
+      skip_empty_lines: true,
+      columns: (header) => header.map((column: string) => snakeToCamel(column)),
+    });
+
+    return parsed;
+  });
+
+  const combinedData = parsedContent.flat();
+
+  return stringToNumber(combinedData);
+}
--- src/pages/api/data/[index].json.ts
+++ src/pages/api/data/[index].json.ts
@@ -1,41 +1,18 @@
 import type { APIRoute } from "astro";
-import { readFileSync } from "node:fs";
-import { parse } from "csv-parse/sync";
 import path from "node:path";
-import type { IndexDefinition } from "src/types/types";
 import { globSync } from "tinyglobby";
+import { loadIndex } from "@data/loaders";
 
 const glob = globSync("src/data/breakpoints/*.csv");
 
-
-const snakeToCamel = (str: string) =>
-  str
-    .toLowerCase()
-    .replace(/([-_][a-z])/g, (group: string) =>
-      group.toUpperCase().replace("-", "").replace("_", "")
-    );
-
 export const GET: APIRoute = async ({ params }) => {
   const index = params.index;
-  const data = readFileSync(`src/data/breakpoints/${index}.csv`);
+  if (!index) {
+    return new Response("Not found", {status: 404 })
+  }
+  const data = loadIndex(index!)
 
-  const parsedContent = parse(data, {
-    skip_empty_lines: true,
-    columns: (header) => header.map((column: string) => snakeToCamel(column)),
-  });
-
-  const stringToNumber: IndexDefinition[] = parsedContent.map(
-    (o: IndexDefinition) => ({
-      ...o,
-      categoryLower: Number(o.categoryLower),
-      categoryUpper: Number(o.categoryUpper),
-      averagingPeriod: Number(o.averagingPeriod),
-      concentrationLower: Number(o.concentrationLower),
-      concentrationUpper: Number(o.concentrationUpper),
-    })
-  );
-
-  return new Response(JSON.stringify(stringToNumber), {
+  return new Response(JSON.stringify(data), {
     headers: { "Content-Type": "application/json" },
   });
 };
